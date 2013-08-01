386011c093f98d9552fcb5228e3355c40f8a9d00
j.vondrous <j.vondrous@localhost>
2013-08-01 16:18:15 +0200

    Maven Compile groovy and Java together.

---
+++ MavenCompileGroovyAndJava/src/main/java/cz/vondrsoft/groovy/JavaClass.java
@@ -0,0 +1,7 @@
+package cz.vondrsoft.groovy;
+
+public class JavaClass {
+    public static void sayHello() {
+        System.out.println("Hello from Java");
+    }
+}
---
+++ MavenCompileGroovyAndJava/src/main/java/net/gmc/groovy/Main.java
@@ -0,0 +1,10 @@
+package net.gmc.groovy;
+
+public class Main {
+    public static final String HELLO = "Hello";
+
+    public static void main(String[] args) {
+        //Call groovy from java
+        new Hello().sayHello();
+    }
+}
