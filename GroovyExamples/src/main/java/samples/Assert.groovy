5ed9883ce3e45d3c8e2707c445c702708cc942c5
j.vondrous <j.vondrous@localhost>
2013-12-06 16:13:27 +0100

    Assertion sample simplification

--- GroovyExamples/src/main/java/samples/Assert.groovy
+++ GroovyExamples/src/main/java/samples/Assert.groovy
@@ -1,8 +1,8 @@
 package samples
 
-assert 2 + 6 == 8
-assert 2 + 6 == 8, "This error message is written when assertion fail"
-assert 2 + 6 == 9: "This error message is written when assertion fail"
+assert 1 + 1 == 2
+assert 1 + 1 == 2, "This error message is written when assertion fail"
+assert 1 + 1 == 2: "This error message is written when assertion fail"
 
 try {
     assert 1 + 1 == 3, "MY OWN ERROR MESSAGE!"
