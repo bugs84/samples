009ff6f79d2695751e873c124eb3a2d853f4b52c
j.vondrous <j.vondrous@localhost>
2014-02-02 18:28:49 +0100

    Vic sampluu viiiic

---
+++ GroovyExamples/src/test/java/presentation/e880/ExecuteCommand.groovy
@@ -0,0 +1,10 @@
+package presentation.e880
+
+import org.codehaus.groovy.runtime.ProcessGroovyMethods
+
+//spust commandline
+def process = "hello.bat".execute()
+// presmeruje out a err procesu do tohot procesu
+ProcessGroovyMethods.consumeProcessOutput(process, System.out, System.err)
+//pockej, az process dojede
+process.waitFor()
