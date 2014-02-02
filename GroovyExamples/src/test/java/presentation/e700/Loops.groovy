30414857ad869fdbd01f47341b20170c9adabd0f
j.vondrous <j.vondrous@localhost>
2014-02-02 17:35:20 +0100

    Loops

--- GroovyExamples/src/test/java/presentation/e700/Loops.groovy
+++ GroovyExamples/src/test/java/presentation/e700/Loops.groovy
@@ -1,3 +1,10 @@
+//tak například times
 def i = 1, list = []
 3.times { list << "${i++}" }
 assert list.join(", ") == "1, 2, 3"
+
+
+//nebo pro tenhle priklad je jeste snad hezci closure nad rosahem
+def list2 = []
+(1..3).each { list2 << "$it" }
+assert list2.join(", ") == "1, 2, 3"
--- GroovyExamples/src/test/java/presentation/eXX/TODO.groovy
+++ GroovyExamples/src/test/java/presentation/eXX/TODO.groovy
@@ -2,14 +2,12 @@ package presentation.eXX
 
 
 //CLOSURES
-//PROPERTIES
+
 //WORK with file
 //XML
 //BUILDER
 //pretypovani
-//iterace
-//Stringy - ',  ", ''', Vkladani promennych
-//Looping - 10.times  (1..10).each ...
+
 
 
 //Equals je defaultni dane na ==, identita je dana na is()
