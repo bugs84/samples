9960d1480b799c2033b3b7a2afea68a3efa2131e
j.vondrous <j.vondrous@localhost>
2014-02-07 16:15:03 +0100

    NamesTest

--- GroovyExamples/src/test/java/presentation/eXX/NamesTest.java
+++ GroovyExamples/src/test/java/presentation/eXX/NamesTest.java
@@ -3,10 +3,12 @@ package presentation.eXX;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.hasItem;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
 public class NamesTest {
 
@@ -19,5 +21,21 @@ public class NamesTest {
     @Test
     public void namesContainsFerda() {
         assertThat(names, hasItem("Ferda"));
+    }
+
+    //TODO odebrat public
+    @Test
+    public void convert() {
+        List<Integer> lengths = getSizes(names);
+        assertThat(lengths, is(Arrays.asList(new Integer[]{4, 5, 7})));
+    }
+
+    private List<Integer> getSizes(List<String> names) {
+        //TODO predelat na collect
+        List<Integer> lengths = new ArrayList<Integer>();
+        for (String name : names) {
+            lengths.add(name.length());
+        }
+        return lengths;
     }
 }
