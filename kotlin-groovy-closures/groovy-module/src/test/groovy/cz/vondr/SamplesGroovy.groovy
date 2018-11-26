82ddbcfd57809c839dff4093a97dc89b6310a874
j.vondrous <j.vondrous@localhost>
2018-11-26 16:36:54 +0100

    GroupBy

--- kotlin-groovy-closures/groovy-module/src/test/groovy/cz/vondr/SamplesGroovy.groovy
+++ kotlin-groovy-closures/groovy-module/src/test/groovy/cz/vondr/SamplesGroovy.groovy
@@ -5,20 +5,39 @@ import org.junit.Test
 class SamplesGroovy {
 
     @Test
-    void "groovy findAll vs kotlin filter"() {
+    void "Groovy findAll vs Kotlin filter"() {
         assert [1, 2, 3, 4, 5].findAll { it < 3 } == [1, 2]
     }
 
     @Test
-    void "groovy find vs kotlin find"() {
+    void "Groovy find vs Kotlin find"() {
         assert [1, 2, 3, 4, 5].find { it < 3 } == 1
     }
 
     @Test
-    void "groovy each vs kotlin foreach"() {
+    void "Groovy each vs Kotlin foreach"() {
         def sum = 0
         [1, 2, 3].each { sum += it }
         assert sum == 6
+    }
+
+    @Test
+    void "Groovy groupBy vs Kotlin groupBy"() {
+        Map<Integer, List<Integer>> groupBy = [1, 2, 3, 4, 5, 6, 7].groupBy { it % 3 }
+        assert groupBy == [
+                0: [3, 6],
+                1: [1, 4, 7],
+                2: [2, 5]
+        ]
+    }
+
+    @Test
+    void "Groovy eachWithIndex vs Kotlin forEachIndexed"() {
+        def result = ""
+        ["A", "B"].eachWithIndex { entry, index ->
+            result += "$index:$entry, "
+        }
+        assert result == "0:A, 1:B, "
     }
 
 
